adbe77cf6615abbaa26700e077b93f1ec58ffbf4
afmireski <afmireski@outlook.com>
2023-03-19 17:10:53 -0300

    #3 - Implement CPU constructor, destructor and calculate

--- calculator.cpp
+++ calculator.cpp
@@ -30,7 +30,54 @@ Operation Keyboard::readOperation() {
 /**
  * Cpu
 */
+Cpu::Cpu() {
+    int memoryLen = 2;
+    this->memory = new float[memoryLen];
+    for (int i = 0; i < memoryLen; i++) this->memory[i] = 0;
 
+    this->operation = Operation::SUM;
+
+    this->writeIndex = 0;
+}
+
+Cpu::~Cpu() {
+    delete this->memory;    
+}
+
+float Cpu::calculate() {
+    float response;
+
+    switch (this->operation)
+    {
+    case SUM:
+        response = this->memory[0] + this->memory[1];
+        break;
+
+    case SUB:
+        response = this->memory[0] - this->memory[1];
+        break;
+    
+    case MLT:
+        response = this->memory[0] * this->memory[1];
+        break;
+
+    case DIV:
+        response = this->memory[0] - this->memory[1];
+        break;
+
+    case RAD:
+        response = this->memory[0] - this->memory[1];
+        break;
+
+    default: // EQU
+        response = this->memory[0];
+        break;
+    }
+    this->memory[0] = response;
+    this->memory[1] = 0;
+
+    return response;
+}
 
 /**
  * Display
