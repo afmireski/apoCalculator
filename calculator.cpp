d8f822c9c93a1f56d0d351fac342d22fe3bb1f5e
afmireski <afmireski@outlook.com>
2023-03-19 16:54:03 -0300

    #3 - Implement Keyboard

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,42 @@
 #include "calculator.hpp"
+#include <iostream>
+using namespace std;
+
+/**
+ * Keyboard
+*/
+Keyboard::Keyboard() {}
+
+Keyboard::~Keyboard() {}
+
+float Keyboard::readNumber() {
+    float input;
+    cin >> input;
+
+    return input;
+}
+
+Operation Keyboard::readOperation() {
+    unsigned char input;
+
+    cin >> input;
+
+    if (input > 6) return Operation::AC;
+
+    return (Operation) input;
+}
+
+
+/**
+ * Cpu
+*/
+
+
+/**
+ * Display
+*/
+
+
+/**
+ * Calculator
+*/
