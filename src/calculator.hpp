ee633951ce611db4ae3527773f597a74b16be7c6
afmireski <afmireski@outlook.com>
2023-03-30 18:30:29 -0300

    #8 - Implement cpu.hpp

--- src/calculator.hpp
+++ src/calculator.hpp
@@ -1,25 +1,5 @@
 #include <interfaces.hpp>
 
-class Cpu : CpuInterface
-{
-    float *memory;
-    Operation operation;
-    unsigned short writeIndex;
-
-public:
-    Cpu();
-
-    ~Cpu();
-
-    float calculate();
-
-    void ac();
-
-    void write(float value);
-
-    void setOperation(Operation value);
-};
-
 class Display : DisplayInterface
 {
 public:
