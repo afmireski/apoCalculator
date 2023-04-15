45a8cb8a0675ad9c402df0600c579566467a0b6c
afmireski <afmireski@outlook.com>
2023-04-15 17:03:18 -0300

    #17 - Implement Register reset

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,49 +1,67 @@
 #include "cpu.hpp"
 #include <cstdlib>
 
-Register::Register() {
+Register::Register()
+{
+    this->reset();
+}
+
+Register::~Register() {}
+
+void Register::updateValue(int value)
+{
+    if (this->hasDecimalSeparator())
+    {
+        this->decimalValue = (this->decimalValue * 10.0) + value;
+    }
+    else
+    {
+        this->intValue = (this->intValue * 10.0) + value;
+    }
+    if (this->bitLen != 1 || value > 0)
+        this->bitLen++;
+}
+
+float Register::getIntValue()
+{
+    return this->intValue;
+}
+
+float Register::getDecimalValue()
+{
+    float value = this->decimalValue;
+
+    while (value > 0.0)
+        value *= 0.1;
+
+    return value;
+}
+
+void Register::setDecimalSeparator(bool value)
+{
+    this->hasSeparator = value;
+}
+
+bool Register::hasDecimalSeparator()
+{
+    return this->hasSeparator;
+}
+
+Signal Register::getSignal()
+{
+    return this->signal;
+}
+
+void Register::setSignal(Signal value = NEGATIVE)
+{
+    this->signal = value;
+}
+
+void Register::reset()
+{
     this->intValue = 0.0;
     this->decimalValue = 0.0;
     this->hasSeparator = false;
     this->signal = POSITIVE;
     this->bitLen = 1;
-}
-
-Register::~Register() {}
-
-void Register::updateValue(int value) {
-    if (this->hasDecimalSeparator()) {
-        this->decimalValue = (this->decimalValue * 10.0) + value;
-    } else {
-        this->intValue = (this->intValue * 10.0) + value;
-    }
-    if (this->bitLen != 1 || value > 0) this->bitLen++;
-}
-
-float Register::getIntValue() {
-    return this->intValue;
-}
-
-float Register::getDecimalValue() {
-    float value = this->decimalValue;
-
-    while(value > 0.0) value *= 0.1;
-
-    return value;
-}
-
-void Register::setDecimalSeparator(bool value) {
-    this->hasSeparator = value;
-}
-
-bool Register::hasDecimalSeparator() {
-    return this->hasSeparator;
-}
-
-Signal Register::getSignal() {
-    return this->signal;
-}
-
-void Register::setSignal(Signal value = NEGATIVE) {
-    this->signal = value;
 }
