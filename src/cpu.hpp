8ab1591401d0a62b0fa61556be854d80e634f932
afmireski <afmireski@outlook.com>
2023-04-15 16:07:16 -0300

    #17 - Define Register

--- src/cpu.hpp
+++ src/cpu.hpp
@@ -1,12 +1,41 @@
 #include "interfaces.hpp"
 
+class Register : public RegisterInterface
+{
+    float intValue;
+    float decimalValue;
+    bool hasDecimalSeparator;
+    Signal signal;
+
+public:
+    Register();
+
+    ~Register();
+
+    void updateValue(float);
+
+    float getIntValue();
+
+    float getDecimalValue();
+
+    void setDecimalSeparator(bool);
+
+    bool hasDecimalSeparator();
+
+    Signal getSignal();
+
+    void setSignal(Signal);
+
+    void reset();
+};
+
 class Cpu : public CpuInterface
 {
     float *memory;
     Operation operation;
     unsigned short writeIndex;
 
-    DisplayInterface* display;
+    DisplayInterface *display;
 
 public:
     Cpu();
@@ -17,5 +46,5 @@ public:
     void receive(Operation);
     void receive(Control);
 
-    void setDisplay(DisplayInterface*);
+    void setDisplay(DisplayInterface *);
 };
